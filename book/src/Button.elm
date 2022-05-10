913243d1fd9b400fd0647ffa930e6e88d0823170
Kamil Trusiak <k.trusiak@outlook.com>
2022-05-10 11:10:39 +0200

    docs: add Button code examples

--- book/src/Button.elm
+++ book/src/Button.elm
@@ -1,8 +1,8 @@
 module Button exposing (..)
 
-import Element exposing (text)
+import Element exposing (..)
 import ElmBook.Actions exposing (logAction)
-import ElmBook.Chapter exposing (chapter, renderComponentList)
+import ElmBook.Chapter exposing (chapter, render, withComponentList)
 import ElmBook.ElmUI exposing (Chapter)
 import Oak.Button
 import Oak.Theme.Default as OakUi
@@ -11,7 +11,7 @@ import Oak.Theme.Default as OakUi
 buttonDocs : Chapter x
 buttonDocs =
     chapter "Button"
-        |> renderComponentList
+        |> withComponentList
             [ ( "Default"
               , OakUi.button []
                     { label = text "Button is Outlined by default"
@@ -37,3 +37,38 @@ buttonDocs =
                     }
               )
             ]
+        |> render """
+```
+import Element exposing (..)
+import Oak.Button
+import Oak.Theme.Default as OakUi
+```
+<component with-label="Default" />
+```
+OakUi.button []
+    { label = text "Button is Outlined by default"
+    , onPress = Just (logAction "Default clicked")
+    }
+```
+<component with-label="Solid" />
+```
+OakUi.button [ Oak.Button.solid ]
+    { label = text "Solid"
+    , onPress = Just (logAction "Solid clicked")
+    }
+```
+<component with-label="Outlined" />
+```
+OakUi.Default.button [ Oak.Button.outlined ]
+    { label = text "Outlined"
+    , onPress = Just (logAction "Outlined clicked")
+    }
+```
+<component with-label="Borderless" />
+```
+OakUi.button [ Oak.Button.borderless ]
+    { label = text "Borderless"
+    , onPress = Just (logAction "Borderless clicked")
+    }
+```
+"""
