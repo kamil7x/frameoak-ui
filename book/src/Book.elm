52abad43c75b9841e8944c03acc582985fa09a4d
Kamil Trusiak <k.trusiak@outlook.com>
2022-05-10 09:39:01 +0200

    chore: update docs url

--- book/src/Book.elm
+++ book/src/Book.elm
@@ -1,11 +1,13 @@
 module Book exposing (main)
 
 import Button exposing (buttonDocs)
-import ElmBook exposing (withChapters)
+import ElmBook exposing (withChapters, withThemeOptions)
 import ElmBook.ElmUI exposing (Book, book)
+import ElmBook.ThemeOptions exposing (routePrefix)
 
 
 main : Book ()
 main =
     book "FrameOak UI"
+        |> withThemeOptions [ routePrefix "/frameoak-ui" ]
         |> withChapters [ buttonDocs ]
